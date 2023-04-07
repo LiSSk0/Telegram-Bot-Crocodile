45ac310f3dd3e064e1bb766537e1bed137c40bbb
ksfortune <105406110+ksfortune@users.noreply.github.com>
2023-04-07 17:38:05 +0500

    +кнопки работают, функции нет

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = ''
+RULES, HELP, START_GAME, STOP, SKIP, RATE = range(6)
+BOT_TOKEN = "6214547917:AAEqMsPS7rEhzuH4xzugdkHiFYGY1v_LzDs"
 
 
-async def help(update, context):
-    await update.message.reply_text("""
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.message.reply_text("""
     ⫸ Команды бота:
     /help - Помощь по боту
     /rules - Правила игры
@@ -25,8 +27,9 @@ async def help(update, context):
     """)
 
 
-async def rules(update, context):
-    await update.message.reply_text("""
+async def rules(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.message.reply_text("""
     ⫸ Правила игры в крокодила:
 
     • Есть ведущий и игроки, которые отгадывают слова.
@@ -40,20 +43,21 @@ async def rules(update, context):
     • За каждое отгаданное слово игрок получает 2 балла, а тот, кто объяснял - 1 балл.
     • В случае, если нечестный ведущий пишет слово-ответ в чат - с него снимаются 3 балла рейтинга.
     """)
+    return 1
 
 
 async def start(update, context):
     keyboard = [
         [
-            InlineKeyboardButton("Правила игры", callback_data='rules_btn'),
-            InlineKeyboardButton("Помощь", callback_data="help_btn"),
+            InlineKeyboardButton("Правила игры", callback_data=str(RULES)),
+            InlineKeyboardButton("Помощь", callback_data=str(HELP)),
 
         ],
-        [InlineKeyboardButton("Играть", callback_data="game_btn"),
-         InlineKeyboardButton("Конец игры", callback_data="stop_btn"),
+        [InlineKeyboardButton("Играть", callback_data=str(START_GAME)),
+         InlineKeyboardButton("Конец игры", callback_data=str(STOP)),
          ],
-        [InlineKeyboardButton("Пропуск ведущего", callback_data="skip_btn"),
-         InlineKeyboardButton("Рейтинг", callback_data="rate_btn"), ]
+        [InlineKeyboardButton("Пропуск ведущего", callback_data=str(SKIP)),
+         InlineKeyboardButton("Рейтинг", callback_data=str(RATE))]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -61,54 +65,60 @@ async def start(update, context):
     await update.message.reply_text(
         """
         ⫸ Игра началась. *игрок* объясняет слово.
-        """, reply_markup=reply_markup)
+        """, reply_markup=reply_markup, )
     return 1
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+async def game(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
+    await query.message.reply_text("""
+    ⫸ game started""")
+    return 1
 
-    await query.answer()
-    await query.message.reply_text(text=f"{query.data}")
 
-
-async def first_response(update, context):
-    locality = update.message.text
-    await update.message.reply_text(f"first response")
+async def rate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.message.reply_text(
+        f"*вывод рейтинга*")
     return 2
 
 
-async def second_response(update, context):
-    weather = update.message.text
-    logger.info(weather)
-    await update.message.reply_text("second response")
-    return ConversationHandler.END
-
-
-async def skip(update, context):
-    await update.message.reply_text(
+async def skip(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.message.reply_text(
         f"*смена ведущего*")
     return 2
 
 
-async def stop(update, context):
-    await update.message.reply_text("⫸ Игра завершена")
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.message.reply_text("⫸ Игра завершена")
     return ConversationHandler.END
-
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("help", help))
-    application.add_handler(CallbackQueryHandler(button))
     application.add_handler(CommandHandler("rules", rules))
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
 
         states={
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
-            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
+            1: [
+                CallbackQueryHandler(rules, pattern="^" + str(RULES) + "$"),
+                CallbackQueryHandler(help, pattern="^" + str(HELP) + "$"),
+                CallbackQueryHandler(game, pattern="^" + str(START_GAME) + "$"),
+                # CallbackQueryHandler(skip, pattern="^" + str(SKIP) + "$"),
+                CallbackQueryHandler(stop, pattern="^" + str(STOP) + "$"),
+                # CallbackQueryHandler(rate, pattern="^" + str(RATE) + "$"),
+
+                # CallbackQueryHandler(three, pattern="^" + str(THREE) + "$"),
+                # CallbackQueryHandler(four, pattern="^" + str(FOUR) + "$"),
+            ],
+            # 1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
+            # 2: []
         },
 
         fallbacks=[CommandHandler('stop', stop), CommandHandler('skip', skip)]
